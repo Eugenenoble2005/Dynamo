5f94f09080072b2ae337c945ea9610b99e2d648d
Eugene Noble <71329328+Eugenenoble2005@users.noreply.github.com>
2023-05-21 14:15:29 +0100

    new commit

--- Dynamo Desktop/Video/Video.axaml.cs
+++ Dynamo Desktop/Video/Video.axaml.cs
@@ -1,7 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using Dynamo_Desktop.ViewModels;
+using FluentAvalonia.UI.Controls;
 using System.Diagnostics;
 
 namespace Dynamo_Desktop.Video;
@@ -13,13 +15,19 @@ public partial class Video : Window
         InitializeComponent();
         DataContext = new VideoViewModel();
         (DataContext as VideoViewModel).Url = Url;
-        (DataContext as VideoViewModel).Play();
+        Opened += (sender, args) =>
+        {
+            VideoViewer.MediaPlayer = (DataContext as VideoViewModel).MediaPlayer;
+            VideoViewer.MediaPlayer.SetHandle(VideoViewer.hndl);
+            (DataContext as VideoViewModel).Play();
+        };
      
     }
     public void StopAllPlayers()
     {
         (DataContext as VideoViewModel).StopPlayer();
     }
+  
     public Video()
     {
         InitializeComponent();  
--- Dynamo Desktop/ViewModels/VideoViewModel.cs
+++ Dynamo Desktop/ViewModels/VideoViewModel.cs
@@ -1,31 +1,67 @@
 ﻿using Avalonia.Controls;
 using LibVLCSharp.Shared;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAvalonia.UI.Controls;
 using System.Net;
 using ReactiveUI;
+using System.Diagnostics;
 
 namespace Dynamo_Desktop.ViewModels
 {
-    internal class VideoViewModel : ViewModelBase
+    public class VideoViewModel : ViewModelBase
     {
 
         private readonly LibVLC _libVlc = new LibVLC();
+        private string _videoTimestamp = "00:00";
+        private long _videoMillSecs = 0;
+        private long _currentMillSecs = 0;
+        private long _previousMillSecs = 0;
+        private string _currentTimestamp = "00:00";
         public string Url { get; set; }
         public MediaPlayer MediaPlayer { get; }
-        private string _playButtonIcon = WebUtility.HtmlDecode("&#xF8AE;");
+        public string _playButtonIcon = WebUtility.HtmlDecode("&#xF8AE;");
+        public string VideoTimeStamp { get => _videoTimestamp; set => this.RaiseAndSetIfChanged(ref _videoTimestamp, value); }
+        public long VideoMillSecs { get => _videoMillSecs; set => this.RaiseAndSetIfChanged(ref _videoMillSecs, value); } 
+        public long CurrentMillSecs { get => _currentMillSecs; set { this.RaiseAndSetIfChanged(ref _currentMillSecs, value);SeekToTime(); } }
+        public string CurrentTimeStamp { get => _currentTimestamp; set => this.RaiseAndSetIfChanged(ref _currentTimestamp, value); }
         public string PlayButtonIcon { get => _playButtonIcon; set => this.RaiseAndSetIfChanged(ref _playButtonIcon, value); }
+        private bool isSeeking = false;
+
         public void Play()
         {
             using var media = new Media(_libVlc, new Uri(Url));
             MediaPlayer.Play(media);
+            MediaPlayer.TimeChanged += (sender, args) =>
+            {
+                TimeSpan timespan = TimeSpan.FromMilliseconds(args.Time);
+                CurrentTimeStamp = timespan.ToString("mm\\:ss");
+                if(isSeeking == false)
+                {
+                    CurrentMillSecs = args.Time;
+                }
+            
+            };
+            media.DurationChanged += (sender, args) =>
+            {
+                TimeSpan timespan = TimeSpan.FromMilliseconds(args.Duration);
+                VideoTimeStamp = timespan.ToString("mm\\:ss");
+                VideoMillSecs = args.Duration;
+            };
            
         }
+        public void SeekToTime()
+        {
+            if (CurrentMillSecs - _previousMillSecs > 1000 || _previousMillSecs - CurrentMillSecs > 1000)
+            {
+                isSeeking = true;
+                TimeSpan timespan = TimeSpan.FromMilliseconds(CurrentMillSecs);
+                    MediaPlayer.SeekTo(timespan);
+                isSeeking = false;
+        
+            }
+            _previousMillSecs = CurrentMillSecs;
+
+        }
+ 
         public void TogglePause()
         {
             MediaPlayer.Pause();
